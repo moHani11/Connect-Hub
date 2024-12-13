445c4845df942c7f5c2826c1a4b7d387f198d139
Asem <asemkilany909@gmail.com>
2024-12-13 02:13:13 +0200

    update notification manager

--- edits/Notification.java
+++ edits/Notification.java
@@ -5,6 +5,7 @@
 package connecthub;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,11 +15,58 @@ import java.util.List;
  * @author Asem
  */
 public class Notification {
-private int notificationId;
-private String message;
-private String userid;
-private String type;
-private Boolean isRead; 
-private LocalDate date;
 
+    private String notificationId;
+    private String message;
+    private String userid;
+    private String type;
+    private Boolean isRead;
+    private LocalDateTime time;
+
+    public Notification(String notificationId, String message, String userid, String type, Boolean isRead, LocalDateTime time) {
+        this.notificationId = notificationId;
+        this.message = message;
+        this.userid = userid;
+        this.type = type;
+        this.isRead = isRead;
+        this.time = time;
+    }
+
+    public String getNotificationId() {
+        return notificationId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Boolean IsRead() {
+        return isRead;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setIsRead(Boolean isRead) {
+        this.isRead = isRead;
+    }
+    
+    
 }
--- edits/NotificationManager.java
+++ edits/NotificationManager.java
@@ -3,27 +3,57 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package connecthub;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+
 /**
  *
  * @author Asem
  */
 public class NotificationManager {
+
     private static final String PATH = "notifications.json";
-    private JSONArray notifications;
- public NotificationManager() throws IOException {
-        this.notifications = loadNotificationsFromFile();
+    private List<Notification> notificationsList;
+
+    public NotificationManager() throws IOException {
+        this.notificationsList = loadNotificationsFromFile();
     }
- public writeNotificationsToFile(){
-     try(FileWriter file =new FileWriter(PATH)){
-         
-     }
- }
+
+    public void writeNotificationsToFile() {
+
+        try (FileWriter file = new FileWriter(PATH)) {
+            JSONArray notiJSONArray = new JSONArray();
+
+            // عمل object من كل اشعار
+            for (Notification noti : notificationsList) {
+                JSONObject JSONnotiObj = new JSONObject();
+                JSONnotiObj.put("notificationId", noti.getNotificationId());
+                JSONnotiObj.put("userId", noti.getUserid());
+                JSONnotiObj.put("type", noti.getType());
+                JSONnotiObj.put("message", noti.getMessage());
+                JSONnotiObj.put("timestamp", noti.getTime().toString());// حولته ل string
+                JSONnotiObj.put("isRead", noti.IsRead());
+                notiJSONArray.add(JSONnotiObj);
+            }
+            // كتابة البيانات إلى الملف في تنسيق JSON
+            file.write(notiJSONArray.toJSONString());  
+            System.out.println("Notifications saved to file successfully!"); 
+
+        } catch (IOException e) {
+            e.printStackTrace();  
+        }
+    }
+    
+    public List<Notification> loadNotificationsFromFile (){
+        List<Notification> loadedNoti = new ArrayList<>();
+    }
 }
+
